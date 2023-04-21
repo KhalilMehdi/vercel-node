89ca5acd407b6afdf50a11108d7aa09178c7ddc1
cbolard <61032251+cbolard@users.noreply.github.com>
2023-04-21 11:23:13 +0200

    up

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/api/ping", (req, res) => {
+  res.send("PONNG");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+module.exports = app;
+

def637c308d34b9422e384256112cb365f75ecf0
cbolard <61032251+cbolard@users.noreply.github.com>
2023-04-21 11:26:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Add Express
 const express = require("express");
+import { StockMovementDto, StockMovementType } from './types';
 
 // Initialize Express
 const app = express();
@@ -7,6 +8,28 @@ const app = express();
 // Create GET request
 app.get("/api/ping", (req, res) => {
   res.send("PONNG");
+});
+
+app.post('/stock/:productId/movement', async (req, res) => {
+  const { productId } = req.params;
+  const { quantity, status } = req.body;
+
+  // Vérifier que le produit existe dans le catalogue
+  try {
+    const response = await fetch("https://microservice-stock.vercel.app/api/products/${productId}");
+    const product = await response.json();
+
+    // Si le produit existe, ajouter la quantité fournie au stock
+    // Si le produit n'existe pas, renvoyer une erreur
+    if (product) {
+      // TODO : Ajouter la quantité fournie au stock
+      res.status(204).send();
+    } else {
+      res.status(400).send(`Le produit n'existe pas dans le catalogue`);
+    }
+  } catch (error) {
+    res.status(500).send('Erreur lors de la vérification du produit');
+  }
 });
 
 // Initialize server

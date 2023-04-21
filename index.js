d4978a74e538679b476dd4384ab7760af30b7e8b
cbolard <61032251+cbolard@users.noreply.github.com>
2023-04-21 13:27:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,15 @@ app.get("/api/ping", (req, res) => {
 });
 
 //GET products
-app.get(`/api/products`, (req, res) => {
+// app.get(`/api/products`, (req, res) => {
 
-  fetch(`https://microservice-stock.vercel.app/api/products`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      res.status(200).send(data);
-    })
-
-}); 
+//   fetch(`https://microservice-stock.vercel.app/api/products`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       res.status(200).send(data);
+//     })
+// }); 
 
 // // Endpoint pour accepter des marchandises dans le stock
 app.post('/api/stock/:productId/movement', async (req, res) => {
